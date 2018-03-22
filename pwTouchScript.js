89863c505ee321874fd5f0d07daf091817a9eabc
aidan8181 <aidan8181@users.noreply.github.com>
2018-03-22 18:55:17 +0000

    Update pwTouchScript.js

--- pwTouchScript.js
+++ pwTouchScript.js
@@ -1,6 +1,5 @@
-$(" *", document.body).click(function (event) {
-       event.stopPropagation();
-       var domElement = $(this).get(0);
-       console.log("TESTING TESTING TESTING");
-       $("span: first").text("Clicked on - " + domElement.nodeName);
-   })();
+$( "*", document.body ).click(function( event ) {
+  event.stopPropagation();
+  var domElement = $( this ).get( 0 );
+  $( "span:first" ).text( "Clicked on - " + domElement.nodeName );
+});
