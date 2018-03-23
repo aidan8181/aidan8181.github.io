c52e1b34e8601780829e8169efec3dd93f009e1d
aidan8181 <aidan8181@gmail.com>
2018-03-23 13:19:41 +0000

    updatingScript

--- pwTouchScriptTesting.js
+++ pwTouchScriptTesting.js
@@ -70,5 +70,10 @@ $( "*", document.body ).click(function( event ) {
   };
 
   console.log(getElementXPath(touchedDomElement));
+  returnDomXPath(touchedDomElement);
+
+  function returnDomXPath(elemenet) {
+        PageMethods.GetItem(elemenet);
+  };
 
 });
