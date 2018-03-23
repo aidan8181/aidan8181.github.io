4104cd9318d2b050e56ea9b64d4c3ada77c5b4f2
aidan8181 <aidan8181@gmail.com>
2018-03-23 17:11:15 +0000

    update

--- pwTouchScriptTesting.js
+++ pwTouchScriptTesting.js
@@ -1,26 +1,26 @@
 $( "*", document.body ).click(async function( event ) {
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+    // function sleep(ms) {
+    //     return new Promise(resolve => setTimeout(resolve, ms));
+    // }
   // prevent parent DOM elelments being notified of the touch event
   event.stopPropagation();
   // create an array to hold the parent elements of the touched DOM item
-  var elementParents = [];
+//   var elementParents = [];
 
  // Traverse the DOM and find all parrent elements of the touch DOM element
  // Note: this section may is no longer needed ensure to remove for final version!! currently usefull for chrome debuggin
-  $(this).parents().addBack().not('html').each(function () {
-    var entry = this.tagName.toLowerCase();
-    if (this.className) {
-      entry += "." + this.className.replace(/ /g, '.');
-    }
-    elementParents.push(entry); // Add element to array
-  });
+//   $(this).parents().addBack().not('html').each(function () {
+//     var entry = this.tagName.toLowerCase();
+//     if (this.className) {
+//       entry += "." + this.className.replace(/ /g, '.');
+//     }
+//     elementParents.push(entry); // Add element to array
+//   });
 
-  console.log(elementParents); // Print array to console Note: for debugging
+//   console.log(elementParents); // Print array to console Note: for debugging
 
-  await sleep(4500);
+  //await sleep(4500);
   var touchedDomElement = $( this ).get( 0 ); // Get the touch DOM element from the document
 
    //touchedDomElement = $( this ).get( 0 ); // Get the touch DOM element from the document
