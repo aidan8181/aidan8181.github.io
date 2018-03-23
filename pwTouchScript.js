c45840d3f563e7d4c50a5e2448c16b10c2a331f7
aidan8181 <aidan8181@gmail.com>
2018-03-23 09:51:30 +0000

    pathfinding Update

--- pwTouchScript.js
+++ pwTouchScript.js
@@ -1,5 +1,17 @@
 $( "*", document.body ).click(function( event ) {
   event.stopPropagation();
+
+  var domElementParents = [];
+  $(this).parents().addBack().not('html').each(function () {
+    var entry = this.tagName.toLowerCase();
+    if (this.className) {
+      entry += "." + this.className.replace(/ /g, '.');
+    }
+    domElementParents.push(entry);
+  });
+
+  console.log(domElementParents);
+
   var domElement = $( this ).get( 0 );
   var pwScriptReturnPara = document.getElementById("pwScriptReturn");
   $( "span:first" ).text( "Clicked on - " + domElement.nodeName );
