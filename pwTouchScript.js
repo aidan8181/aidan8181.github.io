9f8045b66b401efe508cde21272ec54dd8bbebd4
aidan8181 <aidan8181@gmail.com>
2018-03-23 10:12:03 +0000

    xpath update

--- pwTouchScript.js
+++ pwTouchScript.js
@@ -16,5 +16,8 @@ $( "*", document.body ).click(function( event ) {
   var pwScriptReturnPara = document.getElementById("pwScriptReturn");
   $( "span:first" ).text( "Clicked on - " + domElement.nodeName );
   pwScriptReturnPara.innerHTML = domElement.nodeName;
+
+  var xPATH = getPathTo(jQuery(domElement.nodeName).first()[0]);
   console.log(domElement.innerHTML);
+  console.log(xPATH);
 });
