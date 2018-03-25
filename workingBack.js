91b850be2b160b84dc0dd639f5e15c32c532176c
aidan8181 <aidan8181@gmail.com>
2018-03-25 13:16:44 +0100

    update

--- workingBack.js
+++ workingBack.js
@@ -4,7 +4,7 @@
         var longPress = 10000;
         var start;
 
-        $("*", document.body).mouseenter(function() 
+        $("*", document.body).mousedown(function() 
         {
             start = new Date().getTime();
         });
